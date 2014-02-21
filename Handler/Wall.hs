61980a698dd9f542770aca7bc0cc882d90aa3c08
Tamas Laszlo Fabian <giganetom@gmail.com>
2014-02-21 08:20:38 +0100

    Added rudimentary validation

--- Handler/Wall.hs
+++ Handler/Wall.hs
@@ -2,11 +2,27 @@ module Handler.Wall where
 
 import Import
 import State
+import Data.Text
 
 postForm :: Maybe Post -> Html -> MForm Handler (FormResult Post, Widget)
 postForm post = renderDivs $ Post
-    <$> areq textField "Nick" (postNick <$> post)
-    <*> areq textareaField "Post" (postBody <$> post)
+    <$> areq nickField "Nick" (postNick <$> post)
+    <*> areq postField "Post" (postBody <$> post)
+  where
+    nickField = check validateNick textField
+    postField = check validatePost textareaField
+
+    nickLengthError = "Nick must be at most 50 characters long." :: Text
+    postLengthError = "Post must be at most 200 characters long." :: Text
+
+    validateNick n
+        | Data.Text.length n > 50 = Left nickLengthError
+        | otherwise               = Right n
+
+    --validatePost :: Textarea -> Either Text Textarea
+    validatePost p
+        | Data.Text.length (unTextarea p) > 200 = Left postLengthError
+        | otherwise               = Right p
 
 getWallR :: Text -> Handler Html
 getWallR key = do
@@ -33,11 +49,15 @@ postWallR key = do
 
     yesod <- getYesod
 
-    let FormSuccess post = result
-    let Post nick _ = post
-
-    setSession "nick" nick
-
-    liftIO $ addPost (posts yesod) key post
-
-    redirect (WallR key)
+    case result of
+        FormSuccess post -> do
+            let Post nick _ = post
+            setSession "nick" nick
+            liftIO $ addPost (posts yesod) key post
+            redirect (WallR key)
+        FormFailure err -> do
+            setMessage $ toHtml ("Form error: " ++ show (err !! 0))
+            redirect (WallR key)
+        FormMissing -> do
+            setMessage "No form data"
+            redirect (WallR key)
--- State.hs
+++ State.hs
@@ -7,16 +7,16 @@ import Data.Time
 addPost :: TVar PostMap -> Text -> Post -> IO ()
 addPost tvpostmap key post = do
     now <- getCurrentTime
-    let expires = addUTCTime (fromIntegral 86400) now -- TODO: config
+    let expires = addUTCTime (fromIntegral (86400 :: Int)) now -- TODO: config
     atomically $ do
         let updater Nothing = Just $ PostList expires [post]
-            updater (Just (PostList t ps)) = Just $ PostList expires (post : ps)
+            updater (Just (PostList _ ps)) = Just $ PostList expires (post : ps)
             in modifyTVar tvpostmap (alter updater key)
 
 getPosts :: TVar PostMap -> Text -> IO PostList
 getPosts tvpostmap key = do
     now <- getCurrentTime
-    let expires = addUTCTime (fromIntegral 86400) now -- TODO: config
+    let expires = addUTCTime (fromIntegral (86400 :: Int)) now -- TODO: config
     atomically $ do
         postmap <- readTVar tvpostmap
         return $ findWithDefault (PostList expires []) key postmap
