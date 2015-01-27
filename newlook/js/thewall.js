885a58fff6ab1a8fbf4d9efc8743a00fb1ba2479
Tamas Laszlo Fabian <giganetom@gmail.com>
2015-01-27 11:19:39 +0100

    Removed annoying download popup

--- newlook/js/thewall.js
+++ newlook/js/thewall.js
@@ -221,8 +221,8 @@ var WallImport = Builder({
 
 var WallExport = Builder({
   init: function(self) {
-    var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-    saveAs(blob, "hello world.txt");
+    //var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+    //saveAs(blob, "hello world.txt");
   }
 });
 
