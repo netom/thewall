4bec1af90b634fc01adefcd6ca94a3c34e19c170
Tamas Laszlo Fabian <giganetom@gmail.com>
2015-01-31 18:19:42 +0100

    Restarted work with emberjs

---
+++ ember/js/controllers/todo_controller.js
@@ -0,0 +1,23 @@
+Todos.TodoController = Ember.ObjectController.extend({
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    }
+  },
+
+  isEditing: false,
+
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
+});
